43ede118daf9b70836c2b85503c9c8299de2ed66
Guns <gunjan.soni2002@gmail.com>
2023-11-22 12:31:31 +0100

    adds mapping for live grep

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -21,7 +21,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_search({ search = vim.fn.input("Grep > ") });
+    require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
 vim.keymap.set("n", "<leader>o", require("telescope.builtin").oldfiles, { desc = '[?] Find recently opened files' })
@@ -37,6 +37,7 @@ end, { desc = '[/] Fuzzily search in current buffer' })
 vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
 vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
 vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>lg", require("telescope.builtin").live_grep, { desc = "[L]ive [G]rep" })
 vim.keymap.set("n", "<leader>bb", require("telescope.builtin").buffers, { desc = "[B]uffers [B]uffer" })
 vim.keymap.set("n", "<leader>bf", require("telescope.builtin").current_buffer_fuzzy_find, { desc = "[B]uffers [F]uzzy" })
 vim.keymap.set("n", "<leader>bh", require("telescope.builtin").oldfiles, { desc = "[B]uffers [H]istory" })
