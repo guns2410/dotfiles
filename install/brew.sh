900e927f1328788cd9dc122263dd3116aae883c9
Guns <gunjan.soni2002@gmail.com>
2024-06-11 10:26:41 +0200

    adds new install script

---
+++ install/brew.sh
@@ -0,0 +1,280 @@
+# Install Homebrew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+brew upgrade
+
+brew tap homebrew/cask-versions
+
+brew install abseil
+brew install aom
+brew install aribb24
+brew install assimp
+brew install autoconf
+brew install autojump
+brew install bat
+brew install boost
+brew install brotli
+brew install c-ares
+brew install ca-certificates
+brew install cairo
+brew install ceres-solver
+brew install certifi
+brew install cfitsio
+brew install cgif
+brew install cjson
+brew install cmake
+brew install codecrafters
+brew install copilot-cli
+brew install curl
+brew install cw
+brew install dav1d
+brew install dbus
+brew install double-conversion
+brew install eigen
+brew install eza
+brew install fd
+brew install ffmpeg@6
+brew install fftw
+brew install flac
+brew install fontconfig
+brew install freetype
+brew install frei0r
+brew install fribidi
+brew install fswatch
+brew install fzf
+brew install gcc
+brew install gd
+brew install gdbm
+brew install gdk-pixbuf
+brew install gettext
+brew install gflags
+brew install ghostscript
+brew install giflib
+brew install gl2ps
+brew install glew
+brew install glib
+brew install glog
+brew install gmp
+brew install gnupg
+brew install gnutls
+brew install gpgme
+brew install graphite2
+brew install graphviz
+brew install gts
+brew install haproxy
+brew install harfbuzz
+brew install hdf5
+brew install highway
+brew install htop
+brew install httpie
+brew install hunspell
+brew install hwloc
+brew install icu4c
+brew install imagemagick
+brew install imath
+brew install isl
+brew install jasper
+brew install jbig2dec
+brew install jpeg
+brew install jpeg-turbo
+brew install jpeg-xl
+brew install jq
+brew install jsoncpp
+brew install julia
+brew install krb5
+brew install lame
+brew install lazygit
+brew install leptonica
+brew install libaec
+brew install libarchive
+brew install libass
+brew install libassuan
+brew install libavif
+brew install libb2
+brew install libbluray
+brew install libde265
+brew install libdicom
+brew install libevent
+brew install libexif
+brew install libgcrypt
+brew install libgit2
+brew install libgpg-error
+brew install libheif
+brew install libidn
+brew install libidn2
+brew install libimagequant
+brew install libksba
+brew install liblqr
+brew install libmatio
+brew install libmicrohttpd
+brew install libmng
+brew install libmpc
+brew install libnghttp2
+brew install libogg
+brew install libomp
+brew install libpng
+brew install libraw
+brew install librist
+brew install librsvg
+brew install libsamplerate
+brew install libsndfile
+brew install libsodium
+brew install libsoxr
+brew install libspng
+brew install libssh
+brew install libssh2
+brew install libtasn1
+brew install libtiff
+brew install libtool
+brew install libunibreak
+brew install libunistring
+brew install libusb
+brew install libuv
+brew install libvidstab
+brew install libvmaf
+brew install libvorbis
+brew install libvpx
+brew install libvterm
+brew install libx11
+brew install libxau
+brew install libxcb
+brew install libxdmcp
+brew install libxext
+brew install libxml2
+brew install libxrender
+brew install libzip
+brew install little-cms2
+brew install lpeg
+brew install lua
+brew install luajit
+brew install luarocks
+brew install luv
+brew install lz4
+brew install lzo
+brew install m4
+brew install mbedtls
+brew install mbedtls@2
+brew install md4c
+brew install metis
+brew install mongocli
+brew install mongodb-atlas-cli
+brew install mongosh
+brew install mozjpeg
+brew install mpdecimal
+brew install mpfr
+brew install mpg123
+brew install msgpack
+brew install ncurses
+brew install neovim
+brew install netcdf
+brew install netpbm
+brew install nettle
+brew install node
+brew install node@16
+brew install npth
+brew install nspr
+brew install nss
+brew install numpy
+brew install nvm
+brew install oniguruma
+brew install open-mpi
+brew install openblas
+brew install opencore-amr
+brew install opencv
+brew install openexr
+brew install openjdk@17
+brew install openjpeg
+brew install openldap
+brew install openlibm
+brew install openslide
+brew install openssl@1.1
+brew install openssl@3
+brew install openvino
+brew install opus
+brew install p11-kit
+brew install p7zip
+brew install pango
+brew install pcre2
+brew install pinentry
+brew install pixman
+brew install pkg-config
+brew install pmix
+brew install podman
+brew install poppler
+brew install postgresql@14
+brew install protobuf
+brew install pugixml
+brew install pyenv
+brew install pyqt
+brew install python-packaging
+brew install python@3
+brew install qt
+brew install rav1e
+brew install readline
+brew install redis
+brew install ripgrep
+brew install rtmpdump
+brew install rubberband
+brew install sdl2
+brew install sesh
+brew install shared-mime-info
+brew install skhd
+brew install snappy
+brew install speedtest-cli
+brew install speex
+brew install sqlite
+brew install srt
+brew install suite-sparse
+brew install svt-av1
+brew install tbb
+brew install telnet
+brew install tesseract
+brew install theora
+brew install tldr
+brew install tmux
+brew install tree
+brew install tree-sitter
+brew install unbound
+brew install unibilium
+brew install utf8cpp
+brew install utf8proc
+brew install uthash
+brew install vips
+brew install vtk
+brew install watch
+brew install webp
+brew install wget
+brew install x264
+brew install x265
+brew install xorgproto
+brew install xvid
+brew install xz
+brew install yabai
+brew install zeromq
+brew install zimg
+brew install zoxide
+brew install zstd
+brew install --cask alacritty
+brew install --cask font-dejavu-sans-mono-nerd-font
+brew install --cask font-droid-sans-mono-nerd-font
+brew install --cask font-hack-nerd-font
+brew install --cask font-inconsolata-lgc-nerd-font
+brew install --cask font-iosevka-nerd-font
+brew install --cask font-monaspace-nerd-font
+brew install --cask font-mononoki-nerd-font
+brew install --cask font-zed-mono-nerd-font
+brew install --cask miniforge
+brew install --cask monitorcontrol
+brew install --cask ngrok
+brew install --cask pdf-squeezer
+brew install --cask sfdx
+brew install --cask stats
+brew install --cask warp
+brew install --cask wezterm
+
+brew install --cask google-chrome
+brew install --cask firefox
+brew install --cask brave-browser
+brew install --cask slack
+brew install --cask docker
