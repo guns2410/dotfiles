f317aefcca395da618c89bafa7532652d3df4b6b
Guns <gunjan.soni2002@gmail.com>
2024-06-11 16:25:51 +0200

    updates to my dotfiles

--- nvim/lua/guns/plugins/fugitive.lua
+++ nvim/lua/guns/plugins/fugitive.lua
@@ -1,5 +1,8 @@
 return {
 	"tpope/vim-fugitive",
+	dependencies = {
+		"tpope/vim-rhubarb",
+	},
 	keys = {
 		{ "<leader>ggs", ":Git<CR>", { noremap = true, silent = true } },
 		{ "<leader>ggd", ":Gdiff<CR>", { noremap = true, silent = true } },
@@ -9,6 +12,7 @@ return {
 		{ "<leader>ggS", ":Gstatus<CR>", { noremap = true, silent = true } },
 		{ "<leader>ggR", ":Gread<CR>", { noremap = true, silent = true } },
 		{ "<leader>ggf", ":Git log -p --follow -- %<CR>", { noremap = true, silent = true } },
+		{ "<leader>ggB", ":GBrowse<CR>", { noremap = true, silent = true } },
 	},
 	config = function()
 		local fugitive = require("fugitive")
