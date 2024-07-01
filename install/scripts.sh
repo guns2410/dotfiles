a0412f5298167df474d2cf488eb38e898e9bfd46
gsoni <gsoni@PC1320.local>
2024-07-01 14:50:45 +0200

    update install scripts

--- install/scripts.sh
+++ install/scripts.sh
@@ -1,3 +1,8 @@
 git clone https://github.com/catppuccin/lazygit.git ~/.config/lazygit-themes
 defaults import com.manytricks.Moom ./Moom.plist
 
+# Alacritty font AppleFontSmoothing
+ddefaults write org.alacritty AppleFontSmoothing -int 0
+
+# show dock faster
+defaults write com.apple.dock autohide-delay -float 0; killall Dock
